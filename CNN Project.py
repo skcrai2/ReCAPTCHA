096d64352a160d829e78441f473f6685134a312a
Susannah <77982454+skcrai2@users.noreply.github.com>
2022-12-15 07:30:13 -0600

    Add files via upload

---
+++ CNN Project.py
@@ -0,0 +1,247 @@
+
+# Import the  Libraries
+
+import numpy as np
+import pandas as pd 
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+import os
+print(os.listdir("/dogCatData"))
+
+# Define Constants
+
+FAST_RUN = False
+IMAGE_WIDTH=128
+IMAGE_HEIGHT=128
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
+
+# Prepare Training Data
+
+filenames = os.listdir("dogCatData/train")
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+
+from IPython.display import display
+display(df.head())
+display(df.tail())
+
+# See total in count (looks like 12,000 of each)
+
+df['category'].value_counts().plot.bar()
+
+# Create Model
+
+# Input Layer: It represent input image data. It will reshape image into single diminsion array. 
+# Example your image is 64x64 = 4096, it will convert to (4096,1) array.
+# Conv Layer: This layer will extract features from image.
+# Pooling Layer: This layerreduce the spatial volume of input image after convolution.
+# Fully Connected Layer: It connect the network from a layer to another layer
+# Output Layer: It is the predicted values layer.
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+model.summary()
+
+# Callbacks
+
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+earlystop = EarlyStopping(patience=10)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=2, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+callbacks = [earlystop, learning_rate_reduction]
+
+# Prepare Data
+
+df["category"] = df["category"].replace({0: 'cat', 1: 'dog'}) 
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+train_df['category'].value_counts().plot.bar()
+validate_df['category'].value_counts().plot.bar()
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=15
+
+# Training Generator
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "dogCatData/train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df, 
+    "dogCatData/train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+# See how it works
+
+example_df = train_df.sample(n=1).reset_index(drop=True)
+example_generator = train_datagen.flow_from_dataframe(
+    example_df, 
+    "dogCatData/train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical'
+)
+
+plt.figure(figsize=(12, 12))
+for i in range(0, 15):
+    plt.subplot(5, 3, i+1)
+    for X_batch, Y_batch in example_generator:
+        image = X_batch[0]
+        plt.imshow(image)
+        break
+plt.tight_layout()
+plt.show()
+
+# Fit Model and Save Model
+
+epochs=3 if FAST_RUN else 50
+history = model.fit_generator(
+    train_generator, 
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
+
+model.save_weights("model.h5")
+
+# Virtualize Training
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+ax1.plot(history.history['loss'], color='b', label="Training loss")
+ax1.plot(history.history['val_loss'], color='r', label="validation loss")
+ax1.set_xticks(np.arange(1, epochs, 1))
+ax1.set_yticks(np.arange(0, 1, 0.1))
+
+ax2.plot(history.history['accuracy'], color='b', label="Training accuracy")
+ax2.plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+ax2.set_xticks(np.arange(1, epochs, 1))
+
+legend = plt.legend(loc='best', shadow=True)
+plt.tight_layout()
+plt.show()
+
+# Prepare Testing Data
+
+test_filenames = os.listdir("dogCatData/test")
+test_df = pd.DataFrame({
+    'filename': test_filenames
+})
+nb_samples = test_df.shape[0]
+
+# Create Testing Generator
+
+test_gen = ImageDataGenerator(rescale=1./255)
+test_generator = test_gen.flow_from_dataframe(
+    test_df, 
+    "dogCatData/test/", 
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    target_size=IMAGE_SIZE,
+    batch_size=batch_size,
+    shuffle=False
+)
+# Predict
+
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))
+test_df['category'] = np.argmax(predict, axis=-1)
+label_map = dict((v,k) for k,v in train_generator.class_indices.items())
+test_df['category'] = test_df['category'].replace(label_map)
+test_df['category'] = test_df['category'].replace({ 'dog': 1, 'cat': 0 })
+
+# Virtualize Results
+
+test_df['category'].value_counts().plot.bar()
+
+# See Predicted Results with Images
+
+sample_test = test_df.head(18)
+sample_test.head()
+plt.figure(figsize=(12, 24))
+for index, row in sample_test.iterrows():
+    filename = row['filename']
+    category = row['category']
+    img = load_img("dogCatData/test/"+filename, target_size=IMAGE_SIZE)
+    plt.subplot(6, 3, index+1)
+    plt.imshow(img)
+    plt.xlabel(filename + '(' + "{}".format(category) + ')' )
+plt.tight_layout()
+plt.show()
+
+# Final Data -> Export
+
+final_df = test_df.copy()
+final_df['id'] = final_df['filename'].str.split('.').str[0]
+final_df['label'] = final_df['category']
+final_df.drop(['filename', 'category'], axis=1, inplace=True)
+final_df.to_csv(r'\Prediction_Dog_Cat.csv', index=False)
